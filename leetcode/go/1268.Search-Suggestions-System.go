9f20ad5ebbc687c72ef49300a98358c367456e34
Golang Uzbekistan <golanguzb70@gmail.com>
2023-08-13 10:07:59 +0500

    add SuggestedProducts

---
+++ leetcode/go/1268.Search-Suggestions-System.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"sort"
+	"strings"
+)
+
+func SuggestedProducts(products []string, searchWord string) [][]string {
+	response := [][]string{}
+	subArr := []string{}
+	leng := len(searchWord)
+	for i := 0; i < leng; i++ {
+		prefix := searchWord[:i+1]
+		subArr = []string{}
+		for _, e := range products {
+			if strings.HasPrefix(e, prefix) {
+				subArr = append(subArr, e)
+			}
+		}
+		sort.StringSlice.Sort(subArr)
+		if len(subArr) >= 3 {
+			response = append(response, subArr[:3])
+		} else {
+			response = append(response, subArr)
+		}
+	}
+
+	return response
+}
