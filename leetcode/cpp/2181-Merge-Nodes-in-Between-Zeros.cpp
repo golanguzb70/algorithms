c7ed35c12bfb77d943052073112923d8dc4e8b07
Golang Uzbekistan <golanguzb70@gmail.com>
2023-10-09 16:54:44 +0500

    solve another problem

---
+++ leetcode/cpp/2181-Merge-Nodes-in-Between-Zeros.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/*
+    Algorithm
+
+1. Start from last gain sum of head.next nodes,  put next = nullptr and return it 
+2. When node value equals to 0 it calculating from scratch. 
+
+Be sure that previes sums don't get lost
+
+example: 
+head = [0,3,1,0,4,5,2,0]
+recursive - 1: 
+for->1: sum = 3,  head = 1, 0, 4, 5, 2, 0
+for->2: sum = 4, head = 0, 4, 5, 2, 0 -> break because val = 0;
+recursive - 2:
+for->1: sum = 4, head=5, 2, 0
+for->2: sum = 9, head= 2, 0
+for->3: sum = 11, head = 0 -> break because val = 0;
+recursive - 3:
+
+
+*/
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        int sum = 0;
+        head = head->next;
+        if (head == nullptr) {
+            return head;
+        }
+        while(head->val != 0) {
+            sum+=head->val;
+            head = head->next;
+        }
+        head->val = sum;
+        head->next = mergeNodes(head);
+        return head;
+    }
+};
+
--- leetcode/cpp/24-swap-nodes-in-pair.cpp
+++ leetcode/cpp/24-swap-nodes-in-pair.cpp
@@ -8,14 +8,23 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    // ListNode() : val(0), next(nullptr) {}
-    // ListNode(int x) : val(x), next(nullptr) {}
-    // ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* temp = head;
+        head = head->next;
+        temp->next = temp->next->next;
+        head->next = temp;
+
+        head->next->next = swapPairs(head->next->next);
+        return head;
     }
 };
